96e9e7c6bd89dd6f47edfebc472fb376ab92d9fe
neojarvis <techadmin@iamneo.ai>
2024-01-11 06:28:12 +0000

    Auto-save-commit-1/11/2024--11:58:11-AM

--- Vehiclebackend/controllers/vacationRentalController.js
+++ Vehiclebackend/controllers/vacationRentalController.js
@@ -40,6 +40,7 @@ const addVacationRental = async (req, res) => {
     const vacationRental = await VacationRental.create(req.body);
     res.status(200).json({ message: 'Vacation rental added successfully' });
   } catch (error) {
+    console.log("error is",error);
     res.status(500).json({ message: error.message });
   }
 };
