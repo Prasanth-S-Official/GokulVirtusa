d5b77f1c10505d0b408d986d251331d3e521865f
neojarvis <techadmin@iamneo.ai>
2024-01-18 16:11:17 +0000

    Auto-save-commit-1/18/2024--9:41:17-PM

--- jobposting/index.js
+++ jobposting/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/jobdb1")
+  .connect("mongodb://127.0.0.1:27017/jobdb")
   .then(() => {
     console.log("Database connected");
     app.listen(8080, () => {
