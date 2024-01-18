68e6f47e0dd100246a1d5c039bf491ed4be27b24
neojarvis <techadmin@iamneo.ai>
2024-01-18 08:18:41 +0000

    Auto-save-commit-1/18/2024--1:48:40-PM

--- jobposting/routers/jobRouter.js
+++ jobposting/routers/jobRouter.js
@@ -4,12 +4,12 @@ const { validateToken } = require("../authUtils");
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get("/jobs", jobController.getAllJobs);
-router.get("/jobs/user/:userId", jobController.getJobsByUserId);
-router.get("/jobs/:jobId", jobController.getJobById);
-router.post("/jobs", jobController.addJob);
-router.put("/jobs/:jobId", jobController.updateJob);
-router.delete("/jobs/:jobId", jobController.deleteJob);
+router.get("/job", jobController.getAllJobs);
+router.get("/job/user/:userId", jobController.getJobsByUserId);
+router.get("/job/:jobId", jobController.getJobById);
+router.post("/job", jobController.addJob);
+router.put("/job/:jobId", jobController.updateJob);
+router.delete("/job/:jobId", jobController.deleteJob);
 router.get('/users', userController.getAllUsers);
 
 module.exports = router;
