6e4afed23259674109cc5a82c7ec69ab796a3425
neojarvis <techadmin@iamneo.ai>
2024-01-19 01:36:13 +0000

    Auto-save-commit-1/19/2024--7:06:14-AM

---
+++ html_css_js/puppeteer-testNew.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
+  const page = await browser.newPage();
+  try {
+    await page.goto('https://8081-fcebccfceabbafdecaababdaaceb.premiumproject.examly.io/admin/dashboard.html');
+    await page.setViewport({
+      width: 1200,
+      height: 1200,
+    });
+
+    // Use Puppeteer to evaluate the media query
+    const isMediaQueryUsed = await page.evaluate(() => {
+      const mediaQuery = window.matchMedia('(max-width: 768px)');
+      return mediaQuery.matches;
+    });
+
+    const expectedMediaQueryUsage = true; // Adjust this based on your expectations
+
+    if (isMediaQueryUsed === expectedMediaQueryUsage) {
+      console.log('TESTCASE:testcase_for_media_query:success');
+    } else {
+      console.log('TESTCASE:testcase_for_media_query:failure');
+    }
+  } catch (e) {
+    console.log("e",e);
+    console.log('TESTCASE:testcase_for_media_query:failure');
+  } finally {
+    await page.close();
+    await browser.close();
+  }
+})();
