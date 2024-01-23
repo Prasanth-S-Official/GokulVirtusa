f64788b756c438eb42303e7db52075c1a63d0fb2
neojarvis <techadmin@iamneo.ai>
2024-01-23 03:12:20 +0000

    Auto-save-commit-1/23/2024--8:42:19-AM

--- frontendreact/src/tests/App.test.js
+++ frontendreact/src/tests/App.test.js
@@ -1,31 +1,130 @@
-import React from "react";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
-import Register from "../components/authComponents/Register";
+/* eslint-disable testing-library/no-container */
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/prefer-screen-queries */
+import { render, screen, fireEvent } from "@testing-library/react";
+import ForgotPassword from "../components/auth/forgotPassword/ForgotPassword";
 
+jest.mock("axios");
 
+jest.mock("../utils/axiosconfig/axiosconfig", () => ({
+  axiosInstance: jest.fn(),
+}));
 
-describe("Register Component", () => {
-  it("renders the Register component", () => {
-    render(<Register />);
-    expect(screen.getByText("Create Account")).toBeInTheDocument();
+jest.mock("react-toast-notifications", () => ({
+  useToasts: () => ({
+    addToast: jest.fn(),
+  }),
+}));
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => jest.fn(),
+}));
+jest.mock("react-router-dom", () => ({
+  Link: ({ to, children }) => <a href={to}>{children}</a>,
+  useNavigate: () => jest.fn(),
+}));
+
+describe("Week4_Day5_For Forgot Password component", () => {
+  it("week4_day5_testcase_to_check_the_rendering_of_forgotpassword_component", () => {
+    render(<ForgotPassword />);
+    expect(screen.getByText("Forgot Password")).toBeInTheDocument();
   });
+  it("week4_day5_testcase_for_initializing_state_for_storing_form_data_and_if_data_matching_with_regex", async () => {
+    render(<ForgotPassword />);
+    // here we test input states with valid data
+    const formData = {
+      email: "test@example.com",
+      newPassword: "Password1!",
+      confirmPassword: "Password1!",
+    };
 
-  it("validates the form and displays error messages", async () => {
-    render(<Register />);
-    
-    // Simulate form submission without filling required fields
-    fireEvent.click(screen.getByText("Register"));
-    
-    // Wait for validation messages to appear
-    await waitFor(() => {
-      expect(screen.getByText("Full Name is required")).toBeInTheDocument();
-      expect(screen.getByText("Email is required")).toBeInTheDocument();
-      expect(screen.getByText("Phone No is required")).toBeInTheDocument();
-      expect(screen.getByText("Password is required")).toBeInTheDocument();
+    const emailInput = screen.getByTestId("emailInput");
+    fireEvent.change(emailInput, { target: { value: formData.email } });
+
+    const newPassword = screen.getByTestId("newPasswordInput");
+    fireEvent.change(newPassword, { target: { value: formData.newPassword } });
+    const confirmPasword = screen.getByTestId("confirmNewPasswordInput");
+    fireEvent.change(confirmPasword, {
+      target: { value: formData.confirmPassword },
     });
 
-    // Add more assertions based on your validation logic
+    // Define your regex pattern
+    const emailRegex = /\S+@\S+\.\S+/;
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    // Check if the input value matches the regex pattern
+    expect(emailRegex.test(emailInput.value)).toBe(true);
+    expect(passwordRegex.test(newPassword.value)).toBe(true);
+    expect(newPassword.value).toBe(confirmPasword.value);
   });
 
-  // Add more test cases for form submission, API calls, etc.
+  it("week4_day5_testcase_for_error_states_if_the_value_is_empty", () => {
+    const utils = render(<ForgotPassword />);
+    // here we test input states with empty data and error states
+    const errors = {
+      email: "Email is required",
+      newPassword: "New Password is required",
+      confirmPassword: "Confirm Password is required",
+    };
+    const formData = {
+      email: "",
+      newPassword: "",
+      confirmPassword: "",
+    };
+    const emailInput = screen.getByTestId("emailInput");
+    fireEvent.change(emailInput, { target: { value: formData.email } });
+
+    const newPasswordInput = screen.getByTestId("newPasswordInput");
+    fireEvent.change(newPasswordInput, {
+      target: { value: formData.newPassword },
+    });
+    const confirmPasswordInput = screen.getByTestId("confirmNewPasswordInput");
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: formData.confirmPassword },
+    });
+
+    const submitButton = utils.container.querySelector(".resetbtnforgot");
+
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText(errors.email)).toBeInTheDocument();
+    expect(screen.getByText(errors.newPassword)).toBeInTheDocument();
+    expect(screen.getByText(errors.confirmPassword)).toBeInTheDocument();
+  });
+
+  it("week4_day5_testcase_for_error_states_if_the_value_is_invalid", () => {
+    const utils = render(<ForgotPassword />);
+    const formData = {
+      email: "testexample.com",
+      newPassword: "Password1",
+      confirmPassword: "Password1!@",
+    };
+    const errors = {
+      email: "Invalid email address",
+      newPassword:
+        "Password must be 8 characters or more and include at least one uppercase, one lowercase, one special character, and one number",
+      confirmPassword: "Passwords do not match",
+    };
+    // here we test input states with All invalid data or inalid inputs
+    const emailInput = screen.getByTestId("emailInput");
+    fireEvent.change(emailInput, { target: { value: formData.email } });
+
+    const newPasswordInput = screen.getByTestId("newPasswordInput");
+    fireEvent.change(newPasswordInput, {
+      target: { value: formData.newPassword },
+    });
+
+    const confirmPasswordInput = screen.getByTestId("confirmNewPasswordInput");
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: formData.confirmPassword },
+    });
+
+    const submitButton = utils.container.querySelector(".resetbtnforgot");
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText(errors.email)).toBeInTheDocument();
+    expect(screen.getByText(errors.newPassword)).toBeInTheDocument();
+    expect(screen.getByText(errors.confirmPassword)).toBeInTheDocument();
+  });
 });
--- frontendreact/src/utils/axiosconfig/axiosconfig.js
+++ frontendreact/src/utils/axiosconfig/axiosconfig.js
@@ -2,17 +2,6 @@ import axios from "axios";
 
 const baseURL = "https://8080-fcebccfceabbafdecaababdaaceb.premiumproject.examly.io";
 
-const urlParams = new URLSearchParams(window.location.search);
-
-// const handleInvalidToken = (message) => {
-//   localStorage.clear();
-
-//   showToasterMessageForInvalidToken(message);
-
-//   setTimeout(() => {
-//     window.location.href = process.env.REACT_APP_SSO_URL;
-//   }, 1000);
-// };
 
 const axiosInstance = axios.create({
   baseURL,
@@ -20,4 +9,3 @@ const axiosInstance = axios.create({
 });
 
 export { axiosInstance };
-// https://github.com/Nikhiii/Incremental_Project_Mern.git
